bdff4a7fb0ab0347be7a6bae04958e3a1f82659f
Rajneesh Kumar <rajk.31052005@gmail.com>
2023-10-09 23:20:57 +0530

    better os-detection

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
+var Name = "Unknown OS";
+if (navigator.userAgent.indexOf("Win") != -1) Name =
+	"Windows OS";
+if (navigator.userAgent.indexOf("Mac") != -1) Name =
+	"Macintosh";
+if (navigator.userAgent.indexOf("Linux") != -1) Name =
+	"Linux OS";
+if (navigator.userAgent.indexOf("Android") != -1) Name =
+	"Android OS";
+if (navigator.userAgent.indexOf("like Mac") != -1) Name =
+	"iOS";
 
-function detectOS() {
-	let userAgent = window.navigator.userAgent,
-		platform = window.navigator.platform,
-		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-		os = null;
-
-	if (macosPlatforms.indexOf(platform) !== -1) {
-		os = 'Mac OS';
-	} else if (iosPlatforms.indexOf(platform) !== -1) {
-		os = 'iOS';
-	} else if (windowsPlatforms.indexOf(platform) !== -1) {
-		os = 'Windows';
-	} else if (/Android/.test(userAgent)) {
-		os = 'Android';
-	} else if (!os && /Linux/.test(platform)) {
-		os = 'Linux';
-	}
-
-	return os;
-}
-
-let useros = detectOS();
-alert("Dear " + useros + " Site Under Construction.");
+alert("Hello " + Name + ",This Site Is Under Construction.")
