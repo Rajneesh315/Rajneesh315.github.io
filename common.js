91e785fd1d02fe8ae1f0ba6a2c7a7ceaf1a8bca9
Rajneesh Kumar <rajk.31052005@gmail.com>
2023-10-10 00:31:40 +0530

    brushing

--- common.js
+++ common.js
@@ -1,4 +1,3 @@
-
 var OSName = "Unknown OS";
 if (navigator.userAgent.indexOf("Win") != -1) OSName = "Win";
 if (navigator.userAgent.indexOf("Mac") != -1) OSName = "Mac";
@@ -6,7 +5,6 @@ if (navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
 if (navigator.userAgent.indexOf("Android") != -1) OSName = "Android";
 if (navigator.userAgent.indexOf("like Mac") != -1) OSName = "iOS";
 console.log('Your OS: ' + OSName);
-
 
 if (OSName === "Win") {
 	document.getElementById("prompt").innerHTML = "> ";
