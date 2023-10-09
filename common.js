a281272ac52a0a9285611c5e0c8f78d1ea7e9ea0
Rajneesh Kumar <rajk.31052005@gmail.com>
2023-10-10 00:17:09 +0530

    prompts

---
+++ common.js
@@ -0,0 +1,19 @@
+
+var OSName = "Unknown OS";
+if (navigator.userAgent.indexOf("Win") != -1) OSName = "Win";
+if (navigator.userAgent.indexOf("Mac") != -1) OSName = "Mac";
+if (navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+if (navigator.userAgent.indexOf("Android") != -1) OSName = "Android";
+if (navigator.userAgent.indexOf("like Mac") != -1) OSName = "iOS";
+console.log('Your OS: ' + OSName);
+
+
+if (OSName === "Win") {
+	document.getElementById("prompt").innerHTML = "> ";
+}
+else if (OSName === "Mac" || OSName === "Linux" || OSName === "Android" || OSName === "iOS") {
+	document.getElementById("prompt").innerHTML = "$ ";
+}
+else {
+	document.getElementById("prompt").innerHTML = "~ ";
+}
